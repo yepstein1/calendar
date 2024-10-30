190ad2a59aa688c2cef19e2769d6e6c3de39bd8b
Yakov Epstein <yaakovepstein@gmail.com>
2024-10-30 13:41:54 -0400

    add two files

--- frontend/src/transformTodo.js
+++ frontend/src/transformTodo.js
@@ -1,11 +1,10 @@
 export function transformTodo(data) {
- 
- 
+
+
     data = data.map(item => {
         item = Object.entries(item).flat();
         //userId added here
         let userId = localStorage.getItem('userId');
-       // userOjbect = JSON.parse(userOjbect)
         item.unshift(userId);
 
         item.unshift('DEFAULT');
